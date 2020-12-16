779dafbce617605d076ef10e992b6ae8dac28cce
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-12-16 16:38:02 -0700

    v-bind lesson exercise

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            description: 'Cozy hugs for you feet'
+            description: 'Cozy hugs for you feet',
+            image: './assets/images/socks_green.jpg',
+            url: 'https://www.twitter.com'
         }
     }
 })
